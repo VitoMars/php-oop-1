9afd440cd15733ef0a5b4cbfd18eeb7f68dc412d
Vito <vito95v@gmail.com>
2021-10-21 17:47:24 +0200

    fix

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 class Movie
 {
-
    public $title;
    public $genre;
    public $country;
    public $year;
 
-   function __construct($_title, $_genre, $_country, $_year,)
+   function __construct($_title, $_genre, $_country, $_year)
    {
       $this->title = $_title;
       $this->genre = $_genre;
@@ -19,9 +18,46 @@ class Movie
    {
       return $this->title;
    }
+
+   public function getGenre()
+   {
+      return $this->genre;
+   }
+
+   public function getCountry()
+   {
+      return $this->country;
+   }
+
+   public function getYear()
+   {
+      return $this->year;
+   }
 }
 
-$rocky = new Movie("RockyBalboa", "Sportivo/Drammatico", "Stati Uniti", 2006);
+$rocky = new Movie("Rocky Balboa", "Sportivo/Drammatico", "Stati Uniti", 2006);
 $creed = new Movie("Creed", "Sportivo/Drammatico", "Stati Uniti", 2015);
 
-var_dump($rocky);
+var_dump($rocky, $creed);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+
+<body>
+   <div>
+      Titolo: <?php echo $rocky->getTitle() ?> <br>
+      Genere: <?php echo $rocky->getGenre() ?> <br>
+      Paese: <?php echo $rocky->getCountry() ?> <br>
+      Anno: <?php echo $rocky->getYear() ?> <br>
+   </div>
+</body>
+
+</html>
